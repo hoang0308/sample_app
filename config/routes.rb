638000c489588a9db027d2dada5ae1cb1f08cde6
hoang0308 <hoangph030898@gmail.com>
2021-10-15 10:54:27 +0700

    update 5.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help' #tao ra 2 root la help_path va help_url :  'http://www.example.com/help'
+  get '/about', to: 'static_pages#about'  #, as: 'hoang'   #(thay doi ten path)
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
